f41bd943f76b58db73d5ce33cb736d33dee03ef1
Jeremiah Diboll <jdiboll@gmail.com>
2017-09-05 16:18:48 -0400

    initial commit. I hate when I forget to commit earlier.

---
+++ Controllers/MarblesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarblesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarblesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class MarblesController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET api/marbles
+        [HttpGet]
+        public MarblesModel Get()
+        {
+            var marble = new MarblesModel();
+            List<MarblesModel> marbles = marble.GetMarbles();
+            Random rnd = new Random();
+            marble = marbles[rnd.Next(0, marbles.Count())];
+            return marble;
+        }
+
+        // POST api/marbles
+        [HttpPost]
+        public List<MarblesModel> Post([FromForm]int id,string color)
+        {
+            var marble = new MarblesModel();
+            List<MarblesModel> marbles = marble.GetMarbles();
+            var newMarble = new MarblesModel
+            {
+                ID = id++,
+                Color = color
+            };
+            marbles.Add(newMarble);
+            Console.WriteLine(newMarble);
+            Console.WriteLine(marbles);
+            return marbles;
+
+        }
+    }
+}
---
+++ Models/MarblesModel.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+
+namespace MarblesAPI.Models
+{
+    public class MarblesModel
+    {
+        public MarblesModel()
+        {
+        }
+
+        public int ID { get; set; }
+        public string Color { get; set; }
+
+        public List<MarblesModel> GetMarbles()
+        {
+            var MarblesList = new List<MarblesModel>();
+            var Red = new MarblesModel
+            {
+                ID = 1,
+                Color = "Red",
+            };
+            var Blue = new MarblesModel
+            {
+                ID = 2,
+                Color = "Blue",
+            };
+            var Yellow = new MarblesModel
+            {
+                ID = 3,
+                Color = "Yellow",
+            };
+            var Green = new MarblesModel
+            {
+                ID = 4,
+                Color = "Green",
+            };
+            var Black = new MarblesModel
+            {
+                ID = 5,
+                Color = "Black",
+            };
+            var Brown = new MarblesModel
+            {
+                ID = 6,
+                Color = "Brown",
+            };
+            var Orange = new MarblesModel
+            {
+                ID = 7,
+                Color = "Orange",
+            };
+            var White = new MarblesModel
+            {
+                ID = 8,
+                Color = "White",
+            };
+            var Gray = new MarblesModel
+            {
+                ID = 9,
+                Color = "Gray",
+            };
+            MarblesList.Add(Red);
+            MarblesList.Add(Blue);
+            MarblesList.Add(Yellow);
+            MarblesList.Add(Green);
+            MarblesList.Add(Black);
+            MarblesList.Add(Brown);
+            MarblesList.Add(Orange);
+            MarblesList.Add(White);
+            MarblesList.Add(Gray);
+            return MarblesList;
+        }
+    }
+}
